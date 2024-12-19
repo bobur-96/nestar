91a19e314961d9df22f25b2ba44bf7e00aed1f81
bobur-96 <bovlogs96@gmail.com>
2024-12-19 17:51:39 +0900

    feat: develop createProperty GraphQL API

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -1,6 +1,27 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { PropertyService } from './property.service';
+import { MemberType } from '../../libs/enums/member.enum';
+import { UseGuards } from '@nestjs/common';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { ObjectId } from 'mongoose';
+import { Property } from './property';
+import { PropertyInput } from './property.input';
+
 @Resolver()
 export class PropertyResolver {
 	constructor(private readonly propertyService: PropertyService) {}
+
+	@Roles(MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Mutation(() => Property)
+	public async createProperty(
+		@Args('input') input: PropertyInput,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Property> {
+		console.log('Mutation createProperty');
+		input.memberId = memberId;
+		return await this.propertyService.createProperty(input);
+	}
 }
